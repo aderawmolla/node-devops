07ad98ccab7bf943a16331691cf5e0b9ada014e6
aderawmolla <mollaaderaw@gmail.com>
2024-10-15 13:39:50 +0300

    install cors

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 3000;
-app.use(cors())
+const port = process.env.PORT || 2000;
 
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello, World! This is my Node.js app running in a Docker container.');
 });
 
 app.get('/test', (req, res) => {
-    res.send('Hello, World! this the test project');
+    res.send('Hello, World! This is the test project.');
 });
 
-
-app.listen(port,'0.0.0.0', () => {
-    console.log(`Server is running at http://localhost:${port}`);
+// Update the log statement to reflect the correct URL
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running at http://0.0.0.0:${port}`);
 });
