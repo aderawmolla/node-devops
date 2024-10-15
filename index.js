a0ecb40bae5cf81e32855ed420585417411179bd
aderawmolla <mollaaderaw@gmail.com>
2024-10-15 13:55:38 +0300

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 2000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
